098644b68dc8d76d85af016349562fcc6fd933d8
Behemoth <simonostini@gmail.com>
2022-08-19 20:00:14 +0200

    udpate guard

--- libs/server/controllers/auth/src/lib/passport/strategy/jwt.strategy.ts
+++ libs/server/controllers/auth/src/lib/passport/strategy/jwt.strategy.ts
@@ -1,14 +1,27 @@
+import { PrismaService } from '@bregenz-bewegt/server-prisma';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(configService: ConfigService) {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    configService: ConfigService,
+    private prismaService: PrismaService
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: configService.get('JWT_SECRET'),
     });
+  }
+
+  async validate(payload: { sub: string; email: string }) {
+    const user = await this.prismaService.user.findUnique({
+      where: { id: payload.sub },
+    });
+
+    delete user.password;
+    return user;
   }
 }
--- libs/server/controllers/user/src/lib/user.controller.ts
+++ libs/server/controllers/user/src/lib/user.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
 import { UserService } from './user.service';
+import { JwtGuard } from '@bregenz-bewegt/server-controllers-auth';
 
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
+  @UseGuards(JwtGuard)
   @Get()
-  root() {
-    return 'user';
+  profile(@Req() req: Request) {
+    return req.user;
   }
 }
