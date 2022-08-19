16ea66b4b8606f91aa43a27c183b1cd1babcbd91
Behemoth <simonostini@gmail.com>
2022-08-19 16:58:59 +0200

    adapt signup dto

--- libs/server/controllers/auth/src/lib/dto/signup.dto.ts
+++ libs/server/controllers/auth/src/lib/dto/signup.dto.ts
@@ -3,6 +3,10 @@ import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 export class SignUpDto {
   @IsEmail()
   @IsNotEmpty()
+  username: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
 
   @IsString()
