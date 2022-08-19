ea8265d2f3ffbb0541605a4872fb88b0249f6c92
Behemoth <simonostini@gmail.com>
2022-08-19 15:34:27 +0200

    update

--- libs/server/prisma/src/lib/prisma.module.ts
+++ libs/server/prisma/src/lib/prisma.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
+@Global()
 @Module({
   controllers: [],
   providers: [PrismaService],
