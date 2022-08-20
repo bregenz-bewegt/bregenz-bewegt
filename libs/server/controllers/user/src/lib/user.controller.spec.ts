6af76ea20fcd5ef090b2c0398a94236cdeca4f29
Behemoth <simonostini@gmail.com>
2022-08-20 13:53:00 +0200

    fixed test

--- libs/server/controllers/user/src/lib/user.controller.spec.ts
+++ libs/server/controllers/user/src/lib/user.controller.spec.ts
@@ -1,3 +1,5 @@
+import { PrismaService } from '@bregenz-bewegt/server-prisma';
+import { ConfigService } from '@nestjs/config';
 import { Test } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
@@ -7,7 +9,7 @@ describe('UserController', () => {
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [UserService, PrismaService, ConfigService],
       controllers: [UserController],
     }).compile();
 
