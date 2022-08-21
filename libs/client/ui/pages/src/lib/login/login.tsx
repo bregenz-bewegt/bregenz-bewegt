4dc8c97e4efe3ad72d2e5125a4819c980dee77c8
Behemoth <simonostini@gmail.com>
2022-08-21 15:23:41 +0200

    cleaned login

--- libs/client/ui/pages/src/lib/login/login.tsx
+++ libs/client/ui/pages/src/lib/login/login.tsx
@@ -1,11 +1,5 @@
 import { ExploreContainer } from '@bregenz-bewegt/client-ui-components';
-import {
-  IonPage,
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonContent,
-} from '@ionic/react';
+import { IonPage, IonContent } from '@ionic/react';
 import './login.scss';
 
 /* eslint-disable-next-line */
@@ -15,11 +9,6 @@ export const Login: React.FC = (props: LoginProps) => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Login</IonTitle>
-          </IonToolbar>
-        </IonHeader>
         <ExploreContainer name="Leaderboard" />
       </IonContent>
     </IonPage>
