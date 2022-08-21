ace3e0edd662ef244a3057c8aced1bc829c2ae94
Behemoth <simonostini@gmail.com>
2022-08-21 15:15:12 +0200

    update jest

--- libs/client/ui/router/jest.config.ts
+++ libs/client/ui/router/jest.config.ts
@@ -6,5 +6,5 @@ export default {
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../../coverage/libs/client/ui/router',
+  coverageDirectory: '../../../../coverage/libs/client',
 };
