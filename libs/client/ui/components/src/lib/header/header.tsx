ba43f88c9cd0b40bbb495f94faa639c9fb2464c0
Behemoth <simonostini@gmail.com>
2022-08-22 12:18:31 +0200

    udpate

--- libs/client/ui/components/src/lib/header/header.tsx
+++ libs/client/ui/components/src/lib/header/header.tsx
@@ -1,5 +1,12 @@
-import { IonAvatar, IonFab, IonFabButton, IonHeader } from '@ionic/react';
+import {
+  IonAvatar,
+  IonFab,
+  IonFabButton,
+  IonHeader,
+  IonIcon,
+} from '@ionic/react';
 import './header.scss';
+import { notifications } from 'ionicons/icons';
 
 /* eslint-disable-next-line */
 export interface HeaderProps {}
@@ -10,8 +17,10 @@ export const Header = (props: HeaderProps) => {
       <IonAvatar className="header__avatar">
         <img src="https://i.pravatar.cc/150?img=3" alt="profile" />
       </IonAvatar>
-      <IonFab className="header__fab" horizontal="end">
-        <IonFabButton className="header__fab__button"></IonFabButton>
+      <IonFab className="header__fab" horizontal="end" color="white">
+        <IonFabButton className="header__fab__button">
+          <IonIcon icon={notifications} />
+        </IonFabButton>
       </IonFab>
     </IonHeader>
   );
