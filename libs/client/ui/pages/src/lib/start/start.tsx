a2b1f0d97999a9a8e3c7483c3e6b28fc0434338b
Behemoth <simonostini@gmail.com>
2022-08-22 15:02:25 +0200

    added client types and display type select

--- libs/client/ui/pages/src/lib/start/start.tsx
+++ libs/client/ui/pages/src/lib/start/start.tsx
@@ -1,10 +1,22 @@
-import { Header } from '@bregenz-bewegt/client-ui-components';
-import { IonContent, IonPage, IonSearchbar, IonText } from '@ionic/react';
 import { useState } from 'react';
+import { Header } from '@bregenz-bewegt/client-ui-components';
+import {
+  IonContent,
+  IonPage,
+  IonSearchbar,
+  IonSelect,
+  IonSelectOption,
+  IonText,
+} from '@ionic/react';
+import { ParkDisplayType } from '@bregenz-bewegt/client/types';
+
 import './start.scss';
 
 export const Start: React.FC = () => {
   const [searchText, setSearchText] = useState<string>('');
+  const [parkDisplayType, setParkDisplayType] = useState<ParkDisplayType>(
+    ParkDisplayType.List
+  );
 
   return (
     <IonPage className="start">
@@ -18,6 +30,14 @@ export const Start: React.FC = () => {
           onIonChange={(e) => setSearchText(e.detail.value ?? searchText)}
           placeholder="Suche nach Spielplätzen"
         ></IonSearchbar>
+        <IonSelect value={ParkDisplayType.List}>
+          <IonSelectOption value={ParkDisplayType.List}>
+            Listenansicht
+          </IonSelectOption>
+          <IonSelectOption value={ParkDisplayType.Map}>
+            Kartenansicht
+          </IonSelectOption>
+        </IonSelect>
       </IonContent>
     </IonPage>
   );
