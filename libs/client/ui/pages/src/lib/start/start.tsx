6fe0673ac8fcccb3ef3bda8f698f2f523285b513
Behemoth <simonostini@gmail.com>
2022-08-22 14:40:19 +0200

    search query

--- libs/client/ui/pages/src/lib/start/start.tsx
+++ libs/client/ui/pages/src/lib/start/start.tsx
@@ -1,16 +1,22 @@
 import { Header } from '@bregenz-bewegt/client-ui-components';
 import { IonContent, IonPage, IonSearchbar, IonText } from '@ionic/react';
+import { useState } from 'react';
 import './start.scss';
 
 export const Start: React.FC = () => {
+  const [searchQuery, setSearchQuery] = useState<string>('');
+
   return (
-    <IonPage>
+    <IonPage className="start">
       <Header></Header>
-      <IonContent fullscreen>
+      <IonContent fullscreen className="start__content">
         <IonText>
           <h2>Spielplätze</h2>
         </IonText>
-        <IonSearchbar></IonSearchbar>
+        <IonSearchbar
+          value={searchQuery}
+          placeholder="Suche nach Spielplätzen"
+        ></IonSearchbar>
       </IonContent>
     </IonPage>
   );
