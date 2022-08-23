ef80d968296cd5c464f8e06eaa94077649a165bf
Behemoth <simonostini@gmail.com>
2022-08-23 12:10:42 +0200

    update seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,10 @@
 import { PrismaClient } from '@prisma/client';
-
 const prisma = new PrismaClient();
+import * as argon from 'argon2';
 
-const main = async () => {
+const createUsers = async () => {
+  await prisma.user.deleteMany();
+  const password = await argon.hash('some-secret-password');
   await prisma.user.createMany({
     data: [
       {
@@ -12,12 +14,49 @@ const main = async () => {
         firstname: 'Simon',
         lastname: 'Ostini',
         role: 'USER',
-        password: 'my-secred-password',
+        password: password,
         profilePicture: 'path/to/picture',
         coins: 100,
       },
     ],
   });
+};
+
+const createParks = async () => {
+  await prisma.park.deleteMany();
+  await prisma.park.createMany({
+    data: [
+      {
+        name: 'Rieden Vorkloster',
+        address: 'Rotfarbgasse 14a, 6900 Bregenz',
+        image: 'https://picsum.photos/400/200',
+        qr: 'not-yet-defined-0',
+      },
+      {
+        name: 'Rieden Vorkloster',
+        address: 'Rotfarbgasse 14a, 6900 Bregenz',
+        image: 'https://picsum.photos/400/200',
+        qr: 'not-yet-defined-1',
+      },
+      {
+        name: 'Rieden Vorkloster',
+        address: 'Rotfarbgasse 14a, 6900 Bregenz',
+        image: 'https://picsum.photos/400/200',
+        qr: 'not-yet-defined-2',
+      },
+      {
+        name: 'Rieden Vorkloster',
+        address: 'Rotfarbgasse 14a, 6900 Bregenz',
+        image: 'https://picsum.photos/400/200',
+        qr: 'not-yet-defined-3',
+      },
+    ],
+  });
+};
+
+const main = async () => {
+  await createUsers();
+  await createParks();
 };
 
 main()
