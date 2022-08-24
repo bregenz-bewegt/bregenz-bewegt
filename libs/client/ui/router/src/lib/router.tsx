fd7f1c45142243fd0d01061a494b76efd79fb16e
Behemoth <simonostini@gmail.com>
2022-08-24 22:36:19 +0200

    update fab

--- libs/client/ui/router/src/lib/router.tsx
+++ libs/client/ui/router/src/lib/router.tsx
@@ -61,30 +61,32 @@ export const Router: React.FC<RouterProps> = inject(userStore.storeKey)(
           </Route>
         </IonRouterOutlet>
         {displayTabs && (
-          <IonTabs>
-            <IonTabBar slot="bottom">
-              {Object.values(tabRoutes).map((page, i) => {
-                if (page.label !== 'Scan') {
-                  return (
-                    <IonTabButton tab={page.route} href={page.route} key={i}>
-                      <IonIcon icon={page.icon} />
-                      <IonLabel>{page.label}</IonLabel>
-                    </IonTabButton>
-                  );
-                } else {
-                  return (
-                    <IonTabButton disabled tab={page.route}></IonTabButton>
-                  );
-                }
-              })}
-            </IonTabBar>
-          </IonTabs>
+          <>
+            <IonTabs>
+              <IonTabBar slot="bottom">
+                {Object.values(tabRoutes).map((page, i) => {
+                  if (page.label !== 'Scan') {
+                    return (
+                      <IonTabButton tab={page.route} href={page.route} key={i}>
+                        <IonIcon icon={page.icon} />
+                        <IonLabel>{page.label}</IonLabel>
+                      </IonTabButton>
+                    );
+                  } else {
+                    return (
+                      <IonTabButton disabled tab={page.route}></IonTabButton>
+                    );
+                  }
+                })}
+              </IonTabBar>
+            </IonTabs>
+            <IonFab vertical="bottom" horizontal="center" slot="fixed">
+              <IonFabButton href={tabRoutes.scan.route}>
+                <IonIcon icon={scan} />
+              </IonFabButton>
+            </IonFab>
+          </>
         )}
-        <IonFab vertical="bottom" horizontal="center" slot="fixed">
-          <IonFabButton href={tabRoutes.scan.route}>
-            <IonIcon icon={scan} />
-          </IonFabButton>
-        </IonFab>
       </IonReactRouter>
     );
   })
