a7e6581ebd1f955a066f632a5ed8068e67ed54d2
Behemoth <simonostini@gmail.com>
2022-08-24 23:30:03 +0200

    update

--- libs/client/ui/components/src/lib/route-guard/route-guard.tsx
+++ libs/client/ui/components/src/lib/route-guard/route-guard.tsx
@@ -1,7 +1,6 @@
 import { userStore, UserStore } from '@bregenz-bewegt/client/common/stores';
-import { IonSpinner } from '@ionic/react';
 import { inject, observer } from 'mobx-react';
-import { ReactElement, useEffect, useState } from 'react';
+import { ReactElement } from 'react';
 import { Redirect } from 'react-router-dom';
 import './route-guard.scss';
 
@@ -13,16 +12,6 @@ export interface RouteGuardProps {
 
 export const RouteGuard: React.FC<RouteGuardProps> = inject(userStore.storeKey)(
   observer(({ children, userStore }: RouteGuardProps) => {
-    // const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
-    // const [isLoading, setIsLoading] = useState<boolean>(true);
-
-    // useEffect(() => {
-    //   userStore?.checkIfLoggedIn().then((isLoggedIn) => {
-    //     setIsAuthenticated(isLoggedIn);
-    //     setIsLoading(false);
-    //   });
-    // }, []);
-
     return userStore?.isLoggedIn ? children : <Redirect to="/login" />;
   })
 );
