8fd0549358ef489c8e0576e8a18aa2d16f792a24
Behemoth <simonostini@gmail.com>
2022-08-24 14:24:12 +0200

    update

--- libs/client/ui/pages/src/lib/login/login.tsx
+++ libs/client/ui/pages/src/lib/login/login.tsx
@@ -1,3 +1,4 @@
+import { UserStore, userStore } from '@bregenz-bewegt/client/common/stores';
 import { LoginCredentials } from '@bregenz-bewegt/client/types';
 import {
   IonPage,
@@ -5,77 +6,110 @@ import {
   IonText,
   IonButton,
   IonInput,
+  IonLabel,
+  IonSpinner,
 } from '@ionic/react';
+import { inject, observer } from 'mobx-react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './login.scss';
 
 /* eslint-disable-next-line */
-export interface LoginProps {}
+export interface LoginProps {
+  userStore?: UserStore;
+}
 
-export const Login: React.FC = (props: LoginProps) => {
-  const [credentials, setCredentials] = useState<LoginCredentials>({
-    email: '',
-    password: '',
-  });
+export const Login: React.FC<LoginProps> = inject(userStore.storeKey)(
+  observer(({ userStore }) => {
+    const [credentials, setCredentials] = useState<LoginCredentials>({
+      email: '',
+      password: '',
+    });
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  console.log(credentials);
+    const handleLogin = (
+      e: React.MouseEvent<HTMLIonButtonElement, MouseEvent>
+    ) => {
+      console.log(e);
+      setIsLoading(true);
+      setTimeout(() => setIsLoading(false), 1000);
+      // userStore?.login(credentials.email, credentials.password);
+    };
 
-  return (
-    <IonPage className="login">
-      <IonContent className="login__content" fullscreen>
-        <div className="login__flex-wrapper">
-          <IonText className="login__content__title">
-            <h1>
-              Bregenz
-              <br />
-              Bewegt
-            </h1>
-          </IonText>
-          <div className="login__content__login">
-            <IonText>
-              <h2>Anmelden</h2>
+    console.log(credentials);
+
+    return (
+      <IonPage className="login">
+        <IonContent className="login__content" fullscreen>
+          <div className="login__flex-wrapper">
+            <IonText className="login__content__title">
+              <h1>
+                Bregenz
+                <br />
+                Bewegt
+              </h1>
             </IonText>
-            <IonInput
-              value={credentials.email}
-              type="email"
-              inputMode="email"
-              placeholder="Email"
-              name="email"
-              required
-              onIonChange={(e) =>
-                setCredentials((prev) => ({
-                  ...prev,
-                  email: e.detail.value ?? credentials?.email,
-                }))
-              }
-            ></IonInput>
-            <IonInput
-              value={credentials.password}
-              type="password"
-              inputMode="text"
-              placeholder="Passwort"
-              name="password"
-              required
-              onIonChange={(e) =>
-                setCredentials((prev) => ({
-                  ...prev,
-                  password: e.detail.value ?? credentials?.password,
-                }))
-              }
-            ></IonInput>
-            <Link className="login__content__login__forgot-password" to={'#'}>
-              Passwort vergessen?
-            </Link>
-            <IonButton expand="block" color="primary">
-              Anmelden
-            </IonButton>
-            <IonButton expand="block" color="primary" fill="outline">
-              Neu Registrieren
-            </IonButton>
+            <div className="login__content__login">
+              <IonText>
+                <h2>Anmelden</h2>
+              </IonText>
+              <IonInput
+                value={credentials.email}
+                type="email"
+                inputMode="email"
+                placeholder="Email"
+                name="email"
+                required
+                onIonChange={(e) =>
+                  setCredentials((prev) => ({
+                    ...prev,
+                    email: e.detail.value ?? credentials?.email,
+                  }))
+                }
+              ></IonInput>
+              <IonInput
+                value={credentials.password}
+                type="password"
+                inputMode="text"
+                placeholder="Passwort"
+                name="password"
+                required
+                onIonChange={(e) =>
+                  setCredentials((prev) => ({
+                    ...prev,
+                    password: e.detail.value ?? credentials?.password,
+                  }))
+                }
+              ></IonInput>
+              <Link className="login__content__login__forgot-password" to={'#'}>
+                Passwort vergessen?
+              </Link>
+              <IonButton
+                expand="block"
+                color="primary"
+                onClick={(e) => handleLogin(e)}
+                disabled={isLoading}
+              >
+                {isLoading ? (
+                  <IonLabel>
+                    <IonSpinner>Anmelden</IonSpinner>
+                  </IonLabel>
+                ) : (
+                  'Anmelden'
+                )}
+              </IonButton>
+              <IonButton
+                expand="block"
+                color="primary"
+                fill="outline"
+                href="/register"
+              >
+                Neu Registrieren
+              </IonButton>
+            </div>
           </div>
-        </div>
-      </IonContent>
-    </IonPage>
-  );
-};
+        </IonContent>
+      </IonPage>
+    );
+  })
+);
