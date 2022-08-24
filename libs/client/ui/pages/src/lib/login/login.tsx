57884582e2de89f0b9334d78a173cbd8e366424a
Behemoth <simonostini@gmail.com>
2022-08-24 13:22:53 +0200

    update login

--- libs/client/ui/pages/src/lib/login/login.tsx
+++ libs/client/ui/pages/src/lib/login/login.tsx
@@ -1,4 +1,10 @@
-import { IonPage, IonContent, IonText } from '@ionic/react';
+import {
+  IonPage,
+  IonContent,
+  IonText,
+  IonButton,
+  IonInput,
+} from '@ionic/react';
 import './login.scss';
 
 /* eslint-disable-next-line */
@@ -8,13 +14,29 @@ export const Login: React.FC = (props: LoginProps) => {
   return (
     <IonPage className="login">
       <IonContent className="login__content" fullscreen>
-        <IonText className="login__title">
-          <h1>
-            Bregenz
-            <br />
-            Bewegt
-          </h1>
-        </IonText>
+        <div className="login__flex-wrapper">
+          <IonText className="login__content__title">
+            <h1>
+              Bregenz
+              <br />
+              Bewegt
+            </h1>
+          </IonText>
+          <div className="login__content__login">
+            <IonText>
+              <h2>Anmelden</h2>
+            </IonText>
+
+            <IonInput></IonInput>
+            <IonInput></IonInput>
+            <IonButton expand="block" color="primary">
+              Anmelden
+            </IonButton>
+            <IonButton expand="block" color="primary" fill="outline">
+              Neu Registrieren
+            </IonButton>
+          </div>
+        </div>
       </IonContent>
     </IonPage>
   );
