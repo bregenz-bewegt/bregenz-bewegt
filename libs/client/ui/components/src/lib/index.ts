eb6915dfa99c5e03a88467e9d855e54f354edf26
Behemoth <simonostini@gmail.com>
2022-08-25 19:19:33 +0200

    updaet

--- libs/client/ui/components/src/lib/index.ts
+++ libs/client/ui/components/src/lib/index.ts
@@ -2,3 +2,4 @@ export * from './header/header';
 export * from './park-card/park-card';
 export * from './store-provider/store-provider';
 export * from './route-guard/route-guard';
+export * from './input/input';
---
+++ libs/client/ui/components/src/lib/input/input.tsx
@@ -0,0 +1,57 @@
+import './input.scss';
+import { IonInput, IonItem, IonLabel, IonText } from '@ionic/react';
+import {
+  InputChangeEventDetail,
+  IonInputCustomEvent,
+  TextFieldTypes,
+} from '@ionic/core';
+
+export interface InputProps {
+  value?: string | number | null;
+  type?: TextFieldTypes;
+  inputMode?:
+    | 'search'
+    | 'text'
+    | 'email'
+    | 'tel'
+    | 'url'
+    | 'none'
+    | 'numeric'
+    | 'decimal';
+  placeholder?: string;
+  name?: string;
+  required?: boolean;
+  label?: string;
+  error?: string;
+  onIonChange?: (event: IonInputCustomEvent<InputChangeEventDetail>) => void;
+}
+
+export const Input: React.FC<InputProps> = ({
+  value,
+  type,
+  inputMode,
+  placeholder,
+  name,
+  required,
+  label,
+  error,
+  onIonChange,
+}) => {
+  const inputProps = {
+    value,
+    type,
+    inputMode,
+    placeholder,
+    name,
+    required,
+    onIonChange,
+  };
+
+  return (
+    <IonItem>
+      {label && <IonLabel>{label}</IonLabel>}
+      <IonInput {...inputProps}></IonInput>
+      {error && <IonText color="danger">{error}</IonText>}
+    </IonItem>
+  );
+};
