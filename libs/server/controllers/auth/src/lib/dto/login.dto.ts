a9b3b64afda166889e3ba37a4d5f92b61d1f3845
Behemoth <simonostini@gmail.com>
2022-08-26 17:30:13 +0200

    udpate swagger doc

--- libs/server/controllers/auth/src/lib/dto/login.dto.ts
+++ libs/server/controllers/auth/src/lib/dto/login.dto.ts
@@ -1,10 +1,13 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
+  @ApiProperty()
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   password: string;
