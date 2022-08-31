ef8edce8fea2e5ca804742074b358ca9254177e2
Behemoth <simonostini@gmail.com>
2022-08-31 14:24:44 +0200

    update

--- libs/client/ui/components/src/lib/exercise-card/exercise-card.tsx
+++ libs/client/ui/components/src/lib/exercise-card/exercise-card.tsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './exercise-card.scss';
-import { IonCard } from '@ionic/react';
+import {
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
+  IonSkeletonText,
+} from '@ionic/react';
+import { Exercise } from '@bregenz-bewegt/client/types';
+import { title } from 'process';
 
-export interface ExerciseCardProps {}
+export interface ExerciseCardProps extends Exercise {
+  link: string;
+  isLoading?: string;
+}
 
-export const ExerciseCard: React.FC<ExerciseCardProps> = ({}) => {
-  return <IonCard routerLink={}></IonCard>;
+export const ExerciseCard: React.FC<ExerciseCardProps> = ({
+  id,
+  name,
+  description,
+  difficulty,
+  points,
+  video,
+  link,
+  isLoading,
+}) => {
+  const [isImageLoaded, setIsImageLoaded] = useState<boolean>(false);
+  const isLoaded = !isLoading && isImageLoaded;
+
+  return (
+    <IonCard routerLink={link}>
+      <IonCard
+        routerLink={isLoaded ? link : undefined}
+        routerDirection="forward"
+      >
+        {!isLoaded && <IonSkeletonText style={{ height: '64px' }} animated />}
+        <IonCardHeader>
+          <IonCardTitle>
+            {isLoaded ? title : <IonSkeletonText animated />}
+          </IonCardTitle>
+        </IonCardHeader>
+        <IonCardContent>
+          {isLoaded ? description : <IonSkeletonText animated />}
+        </IonCardContent>
+      </IonCard>
+    </IonCard>
+  );
 };
