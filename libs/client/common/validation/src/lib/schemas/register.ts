77e698592915fd49216bd723e84efbf98ac5465c
Behemoth <simonostini@gmail.com>
2022-09-02 22:27:20 +0200

    update

--- libs/client/common/validation/src/lib/schemas/register.ts
+++ libs/client/common/validation/src/lib/schemas/register.ts
@@ -3,6 +3,9 @@ import * as yup from 'yup';
 export const registerSchema = yup.object().shape({
   username: yup.string().required(),
   email: yup.string().required().email(),
-  password: yup.string().required().min(8),
+  password: yup
+    .string()
+    .required()
+    .min(8, 'Passwort muss mindestens ${min} Zeichen enthalten'),
   passwordConfirm: yup.string().required(),
 });
