323b09db0f5aa9f50786ec437cc79924462e596c
Behemoth <simonostini@gmail.com>
2022-09-02 17:34:11 +0200

    update

--- libs/client/ui/components/src/lib/coin-depot/coin-depot.tsx
+++ libs/client/ui/components/src/lib/coin-depot/coin-depot.tsx
@@ -1,9 +1,8 @@
 import { userStore, UserStore } from '@bregenz-bewegt/client/common/stores';
-import { IonCard, IonCardContent } from '@ionic/react';
+import { IonCard, IonCardContent, IonCardHeader } from '@ionic/react';
 import { inject, observer } from 'mobx-react';
 import './coin-depot.scss';
 
-/* eslint-disable-next-line */
 export interface CoinDepotProps {
   userStore?: UserStore;
 }
@@ -12,6 +11,7 @@ export const CoinDepot: React.FC<CoinDepotProps> = inject(userStore.storeKey)(
   observer(({ userStore }) => {
     return (
       <IonCard className="coin-depot" color="secondary">
+        <IonCardHeader></IonCardHeader>
         <IonCardContent>test</IonCardContent>
       </IonCard>
     );
