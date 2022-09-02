7c29b8c9116eeb2b6781a0177958b5c4e18e109d
Behemoth <simonostini@gmail.com>
2022-09-02 17:43:11 +0200

    update

--- libs/client/ui/components/src/lib/coin-depot/coin-depot.tsx
+++ libs/client/ui/components/src/lib/coin-depot/coin-depot.tsx
@@ -12,7 +12,9 @@ export const CoinDepot: React.FC<CoinDepotProps> = inject(userStore.storeKey)(
     return (
       <IonCard className="coin-depot" color="secondary">
         <IonCardHeader>
-          <div className="bb-coin"></div>
+          <div className="bb-coin">
+            <h2>BB</h2>
+          </div>
         </IonCardHeader>
         <IonCardContent>test</IonCardContent>
       </IonCard>
