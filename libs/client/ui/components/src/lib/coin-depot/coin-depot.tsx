a2bafa220902e8000e8fdcb14f83702ce17cbdd1
Behemoth <simonostini@gmail.com>
2022-09-02 18:05:44 +0200

    udpate

--- libs/client/ui/components/src/lib/coin-depot/coin-depot.tsx
+++ libs/client/ui/components/src/lib/coin-depot/coin-depot.tsx
@@ -18,7 +18,7 @@ export interface CoinDepotProps {
 export const CoinDepot: React.FC<CoinDepotProps> = inject(userStore.storeKey)(
   observer(({ userStore }) => {
     return (
-      <IonCard className="coin-depot" color="secondary">
+      <IonCard className="coin-depot">
         <IonCardHeader>
           <div className="bb-coin">
             <h1>BB</h1>
