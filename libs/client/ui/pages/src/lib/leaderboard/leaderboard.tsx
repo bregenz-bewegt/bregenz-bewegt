d65094c04d332467c9d89a69044f06c79361ec2b
Behemoth <simonostini@gmail.com>
2022-09-05 09:53:23 +0200

    update

--- libs/client/ui/pages/src/lib/leaderboard/leaderboard.tsx
+++ libs/client/ui/pages/src/lib/leaderboard/leaderboard.tsx
@@ -1,5 +1,5 @@
 import { CoinDepot, Header } from '@bregenz-bewegt/client-ui-components';
-import { IonContent, IonPage } from '@ionic/react';
+import { IonContent, IonPage, IonSearchbar } from '@ionic/react';
 import './leaderboard.scss';
 
 export const Leaderboard: React.FC = () => {
@@ -8,6 +8,7 @@ export const Leaderboard: React.FC = () => {
       <Header />
       <IonContent className="leaderboard" fullscreen>
         <CoinDepot />
+        <IonSearchbar/>
       </IonContent>
     </IonPage>
   );
