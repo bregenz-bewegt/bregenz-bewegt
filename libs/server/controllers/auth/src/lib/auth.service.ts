6b4d8190ed32ae9e3439f7cf7f1a7fb974200c1e
Behemoth <simonostini@gmail.com>
2022-09-05 15:23:11 +0200

    update

--- libs/server/controllers/auth/src/lib/auth.service.ts
+++ libs/server/controllers/auth/src/lib/auth.service.ts
@@ -203,7 +203,9 @@ export class AuthService {
 
   async resetPassword(token: string, dto: ResetPasswordDto) {
     console.log(dto);
-    const tokenValid = this.jwtService.verify(token);
+    const tokenValid = this.jwtService.verify(token, {
+      secret: this.configService.get('NX_PASSWORD_RESET_TOKEN_SECRET'),
+    });
     console.log(tokenValid);
     // const user = await this.prismaService.user.findUnique({});
   }
