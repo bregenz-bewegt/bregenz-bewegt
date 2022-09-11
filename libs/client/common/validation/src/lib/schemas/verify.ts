74a51d58540a1089ed2160fb896411e6b9480817
Behemoth <simonostini@gmail.com>
2022-09-11 18:08:03 +0200

    update

--- libs/client/common/validation/src/lib/schemas/verify.ts
+++ libs/client/common/validation/src/lib/schemas/verify.ts
@@ -3,8 +3,8 @@ import * as yup from 'yup';
 export const verifySchema = yup.object().shape({
   token: yup
     .string()
-    .matches(/^[0-9]+$/)
-    .required()
+    .matches(/^[0-9]+$/, '')
+    .required('')
     .min(6, '')
     .max(6, ''),
 });
