8fbcfb9dbbf43ad1a23c27274924f6554d8c7331
Behemoth <simonostini@gmail.com>
2022-09-11 15:41:18 +0200

    update

--- libs/client/ui/pages/src/lib/verify-email/verify-email.tsx
+++ libs/client/ui/pages/src/lib/verify-email/verify-email.tsx
@@ -91,7 +91,7 @@ export const VerifyEmail: React.FC<VerifyEmailProps> = inject(
                 </IonRow>
                 <OtpInput
                   value={verify.values.otp ?? undefined}
-                  onChange={(value) => verify.setFieldValue('pin', value)}
+                  onChange={(value) => verify.setFieldValue('otp', value)}
                 />
               </div>
               <div className="flex-wrapper__actions">
