4b1c9ada749c07d5709df257d05d90fe9277b1ea
Behemoth <simonostini@gmail.com>
2022-09-15 16:43:54 +0200

    test

--- libs/client/ui/components/src/lib/activity-slider/activity-slider.tsx
+++ libs/client/ui/components/src/lib/activity-slider/activity-slider.tsx
@@ -10,6 +10,7 @@ import {
 } from '@dnd-kit/core';
 import { restrictToParentElement } from '@dnd-kit/modifiers';
 import { ActivityStore } from '@bregenz-bewegt/client/common/stores';
+import { ReactNode, useState } from 'react';
 
 export interface ActivitySliderProps {
   activityStore?: ActivityStore;
@@ -18,14 +19,17 @@ export interface ActivitySliderProps {
 export const ActivitySlider: React.FC<ActivitySliderProps> = ({
   activityStore,
 }) => {
+  const [isLocked, setIsLocked] = useState<boolean>(false);
   const sensors = useSensors(useSensor(TouchSensor), useSensor(MouseSensor));
+
+  const handleMarkup = <Handle />;
 
   return (
     <div className="activity-slider">
       <DndContext sensors={sensors} modifiers={[restrictToParentElement]}>
         <div className="activity-slider__sliding-restrictor">
-          <Handle />
-          <LockingSection />
+          {isLocked && handleMarkup}
+          <LockingSection>{isLocked && handleMarkup}</LockingSection>
         </div>
       </DndContext>
     </div>
@@ -53,12 +57,18 @@ const Handle: React.FC = () => {
   );
 };
 
-const LockingSection: React.FC = () => {
+interface LockingSectionProps {
+  children: ReactNode;
+}
+
+const LockingSection: React.FC<LockingSectionProps> = ({ children }) => {
   const { setNodeRef } = useDroppable({
     id: 'locking-section',
   });
 
   return (
-    <div ref={setNodeRef} className="activity-slider__locking-section"></div>
+    <div ref={setNodeRef} className="activity-slider__locking-section">
+      {children}
+    </div>
   );
 };
