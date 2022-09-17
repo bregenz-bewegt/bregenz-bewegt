ade32a46401d5e79fcb52981a93209ed1a518dcd
Behemoth <simonostini@gmail.com>
2022-09-17 13:31:43 +0200

    fix

--- libs/client/ui/router/src/lib/router.tsx
+++ libs/client/ui/router/src/lib/router.tsx
@@ -23,6 +23,7 @@ import {
 } from '@bregenz-bewegt/client-ui-pages';
 import { inject, observer } from 'mobx-react';
 import {
+  tabStore,
   TabStore,
   UserStore,
   userStore,
@@ -57,90 +58,97 @@ export interface TabsProps {
   tabStore?: TabStore;
 }
 
-export const Tabs: React.FC<TabsProps> = ({ tabStore }) => {
-  return (
-    <>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Switch>
-            <Route
-              exact
-              path={'/reset-password/:token'}
-              component={ResetPassword}
-            ></Route>
-            <Route
-              exact
-              path={`${tabRoutes.start.route}/:park/:exercise`}
-              component={ExerciseDetail}
-            />
-            <Route
-              exact
-              path={`${tabRoutes.start.route}/:park`}
-              component={ParkDetail}
-            />
-            <Route
-              exact
-              path={tabRoutes.start.route}
-              component={tabRoutes.start.component}
-            ></Route>
-            <Route
-              exact
-              path={tabRoutes.leaderboard.route}
-              component={tabRoutes.leaderboard.component}
-            ></Route>
-            <Route
-              exact
-              path={tabRoutes.scan.route}
-              component={tabRoutes.scan.component}
-            ></Route>
-            <Route
-              exact
-              path={tabRoutes.analytics.route}
-              component={tabRoutes.analytics.component}
-            ></Route>
-            <Route
-              exact
-              path={tabRoutes.profile.route}
-              component={tabRoutes.profile.component}
-            ></Route>
-            <Route path="">
-              <Redirect to="/start" />
-            </Route>
-          </Switch>
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom" hidden={!tabStore?.isShown}>
-          {Object.values(tabRoutes).map((page, i) => {
-            if (page.label !== 'Scan') {
-              return (
-                <IonTabButton
-                  mode="ios"
-                  tab={page.route}
-                  href={page.route}
-                  key={i}
-                >
-                  <IonIcon icon={page.icon} />
-                  <IonLabel>{page.label}</IonLabel>
-                </IonTabButton>
-              );
-            } else {
-              return <IonTabButton disabled tab={page.route}></IonTabButton>;
-            }
-          })}
-        </IonTabBar>
-      </IonTabs>
-      <IonFab
-        vertical="bottom"
-        horizontal="center"
-        slot="fixed"
-        hidden={!tabStore?.isShown}
-      >
-        <IonFabButton routerLink={tabRoutes.scan.route} routerDirection="root">
-          <IonIcon icon={scan} />
-        </IonFabButton>
-      </IonFab>
-    </>
-  );
-};
+export const Tabs: React.FC<TabsProps> = inject(tabStore.storeKey)(
+  observer(({ tabStore }) => {
+    console.log(tabStore?.isShown);
+
+    return (
+      <>
+        <IonTabs>
+          <IonRouterOutlet>
+            <Switch>
+              <Route
+                exact
+                path={'/reset-password/:token'}
+                component={ResetPassword}
+              ></Route>
+              <Route
+                exact
+                path={`${tabRoutes.start.route}/:park/:exercise`}
+                component={ExerciseDetail}
+              />
+              <Route
+                exact
+                path={`${tabRoutes.start.route}/:park`}
+                component={ParkDetail}
+              />
+              <Route
+                exact
+                path={tabRoutes.start.route}
+                component={tabRoutes.start.component}
+              ></Route>
+              <Route
+                exact
+                path={tabRoutes.leaderboard.route}
+                component={tabRoutes.leaderboard.component}
+              ></Route>
+              <Route
+                exact
+                path={tabRoutes.scan.route}
+                component={tabRoutes.scan.component}
+              ></Route>
+              <Route
+                exact
+                path={tabRoutes.analytics.route}
+                component={tabRoutes.analytics.component}
+              ></Route>
+              <Route
+                exact
+                path={tabRoutes.profile.route}
+                component={tabRoutes.profile.component}
+              ></Route>
+              <Route path="">
+                <Redirect to="/start" />
+              </Route>
+            </Switch>
+          </IonRouterOutlet>
+          <IonTabBar slot="bottom" hidden={!tabStore?.isShown}>
+            {Object.values(tabRoutes).map((page, i) => {
+              if (page.label !== 'Scan') {
+                return (
+                  <IonTabButton
+                    mode="ios"
+                    tab={page.route}
+                    href={page.route}
+                    key={i}
+                  >
+                    <IonIcon icon={page.icon} />
+                    <IonLabel>{page.label}</IonLabel>
+                  </IonTabButton>
+                );
+              } else {
+                return <IonTabButton disabled tab={page.route}></IonTabButton>;
+              }
+            })}
+          </IonTabBar>
+        </IonTabs>
+        <IonFab
+          vertical="bottom"
+          horizontal="center"
+          slot="fixed"
+          hidden={!tabStore?.isShown}
+        >
+          <IonFabButton
+            routerLink={tabRoutes.scan.route}
+            routerDirection="root"
+          >
+            <IonIcon icon={scan} />
+          </IonFabButton>
+        </IonFab>
+      </>
+    );
+  })
+);
 
 export const PublicRouterOutlet: React.FC = () => {
   return (
