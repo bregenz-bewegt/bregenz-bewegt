9d059d532e7da00bec361edd985d6b2fc1705ef7
Behemoth <simonostini@gmail.com>
2022-09-20 16:46:41 +0200

    added web scanner

--- libs/client/ui/pages/src/lib/scan/scan.tsx
+++ libs/client/ui/pages/src/lib/scan/scan.tsx
@@ -3,38 +3,47 @@ import {
   IonContent,
   IonHeader,
   IonPage,
-  IonRow,
   IonTitle,
   IonToolbar,
 } from '@ionic/react';
 import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner';
+import { QrReader } from 'react-qr-reader';
 import { useEffect, useState } from 'react';
+import { Capacitor } from '@capacitor/core';
 
 export const Scan: React.FC = () => {
   const [qrResult, setQrResult] = useState<string>('');
-  const openScanner = async () => {
+  const [isNative, setIsNative] = useState<boolean>();
+
+  const openNativeScanner = async () => {
     const data = await BarcodeScanner.scan();
     setQrResult(data.text ?? 'test');
   };
 
   useEffect(() => {
-    openScanner();
+    const isNativePlatform = Capacitor.isNativePlatform();
+    setIsNative(isNativePlatform);
+
+    if (isNativePlatform) {
+      openNativeScanner();
+    }
   }, []);
 
   return (
-    <IonPage>
+    <IonPage className="scan">
       <IonHeader>
         <IonToolbar>
           <IonTitle>Scan</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Scan</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonRow>{qrResult}</IonRow>
+        <QrReader
+          className="scan__web-scanner"
+          constraints={{}}
+          onResult={(result, error) => {
+            console.log(result, error);
+          }}
+        />
       </IonContent>
     </IonPage>
   );
