f8160e8941581a15675ec23e3b32a17b3f8bd13e
Behemoth <simonostini@gmail.com>
2022-09-29 22:33:52 +0200

    update

--- libs/client/ui/pages/src/lib/onboarding/slides/intro/intro.tsx
+++ libs/client/ui/pages/src/lib/onboarding/slides/intro/intro.tsx
@@ -17,17 +17,9 @@ export const Intro: React.FC<IntroProps> = (props: IntroProps) => {
   return (
     <IonSlide className="intro">
       <IonGrid>
-        <IonRow className="ion-align-items-end">
-          <IonCol>
-            <TitleBanner animated />
-          </IonCol>
-        </IonRow>
         <IonRow className="ion-align-items-center">
           <IonCol>
-            <IonButton expand="block" fill="clear">
-              Starten
-              <IonIcon slot="end" icon={chevronForward} />
-            </IonButton>
+            <TitleBanner animated />
           </IonCol>
         </IonRow>
       </IonGrid>
