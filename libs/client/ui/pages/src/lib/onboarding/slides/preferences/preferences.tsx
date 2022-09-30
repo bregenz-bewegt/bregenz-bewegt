4e06865cf37fe9f6a635ff5e31c93f168890bcc7
Behemoth <simonostini@gmail.com>
2022-09-30 09:17:14 +0200

    update

--- libs/client/ui/pages/src/lib/onboarding/slides/preferences/preferences.tsx
+++ libs/client/ui/pages/src/lib/onboarding/slides/preferences/preferences.tsx
@@ -1,4 +1,4 @@
-import { IonSlide } from '@ionic/react';
+import { IonCol, IonGrid, IonRow, IonSlide, IonText } from '@ionic/react';
 import './preferences.scss';
 
 /* eslint-disable-next-line */
@@ -8,8 +8,22 @@ export const Preferences: React.FC<PreferencesProps> = (
   props: PreferencesProps
 ) => {
   return (
-    <IonSlide>
-      <h1>Preferences</h1>
+    <IonSlide className="preferences">
+      <IonGrid>
+        <IonRow>
+          <IonCol>
+            <IonText>
+              <h2>Präferenzen auswählen</h2>
+            </IonText>
+            <IonText>
+              <p>Setze deine persönlichen Präferenzen</p>
+            </IonText>
+          </IonCol>
+        </IonRow>
+        <IonRow>
+          <IonCol></IonCol>
+        </IonRow>
+      </IonGrid>
     </IonSlide>
   );
 };
