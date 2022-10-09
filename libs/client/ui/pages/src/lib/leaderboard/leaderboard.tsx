e65fcdfd08b7208363ce42fe95934658d04edfad
Behemoth <simonostini@gmail.com>
2022-10-09 17:53:34 +0200

    Add leaderboard table

--- libs/client/ui/pages/src/lib/leaderboard/leaderboard.tsx
+++ libs/client/ui/pages/src/lib/leaderboard/leaderboard.tsx
@@ -1,13 +1,18 @@
 import { CoinDepot, Header } from '@bregenz-bewegt/client-ui-components';
-import { IonContent, IonPage } from '@ionic/react';
+import { IonContent, IonPage, IonRow, IonText } from '@ionic/react';
 import './leaderboard.scss';
 
 export const Leaderboard: React.FC = () => {
   return (
-    <IonPage>
+    <IonPage className="leaderboard">
       <Header />
       <IonContent className="leaderboard" fullscreen>
         <CoinDepot />
+        <IonRow>
+          <IonText>
+            <h1>Rangliste</h1>
+          </IonText>
+        </IonRow>
       </IonContent>
     </IonPage>
   );
