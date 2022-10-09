fc8569fa5fe850e01a1f0d9ccfbf6387751e50dd
Behemoth <simonostini@gmail.com>
2022-10-09 18:06:40 +0200

    update

---
+++ libs/client/types/src/lib/enums/leaderboard-timespan.ts
@@ -0,0 +1,4 @@
+export enum LeaderboardTimespan {
+  AllTime = 'allTime',
+  Yearly = 'yearly',
+}
--- libs/client/ui/pages/src/lib/leaderboard/leaderboard.tsx
+++ libs/client/ui/pages/src/lib/leaderboard/leaderboard.tsx
@@ -1,17 +1,48 @@
 import { CoinDepot, Header } from '@bregenz-bewegt/client-ui-components';
-import { IonContent, IonPage, IonRow, IonText } from '@ionic/react';
+import { LeaderboardTimespan } from '@bregenz-bewegt/client/types';
+import {
+  IonCol,
+  IonContent,
+  IonPage,
+  IonRow,
+  IonSelect,
+  IonSelectOption,
+  IonText,
+} from '@ionic/react';
+import { useState } from 'react';
 import './leaderboard.scss';
 
 export const Leaderboard: React.FC = () => {
+  const [timespan, setTimespan] = useState<LeaderboardTimespan>(
+    LeaderboardTimespan.AllTime
+  );
+
   return (
     <IonPage className="leaderboard">
       <Header />
       <IonContent className="leaderboard" fullscreen>
         <CoinDepot />
         <IonRow>
-          <IonText>
-            <h2>Rangliste</h2>
-          </IonText>
+          <IonCol>
+            <IonText>
+              <h2>Rangliste</h2>
+            </IonText>
+          </IonCol>
+          <IonCol>
+            <IonSelect
+              interface="popover"
+              value={timespan}
+              className="leaderboard__timespan-select"
+              onIonChange={(e) => setTimespan(e.detail.value)}
+            >
+              <IonSelectOption value={LeaderboardTimespan.AllTime}>
+                Allzeit
+              </IonSelectOption>
+              <IonSelectOption value={LeaderboardTimespan.Yearly}>
+                Jahr
+              </IonSelectOption>
+            </IonSelect>
+          </IonCol>
         </IonRow>
       </IonContent>
     </IonPage>
