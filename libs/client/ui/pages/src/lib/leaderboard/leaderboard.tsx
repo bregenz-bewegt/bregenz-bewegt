2cf91ec6f84aa01d19a31d82f373150f325909ff
Behemoth <simonostini@gmail.com>
2022-10-09 18:54:08 +0200

    update

--- libs/client/ui/pages/src/lib/leaderboard/leaderboard.tsx
+++ libs/client/ui/pages/src/lib/leaderboard/leaderboard.tsx
@@ -47,16 +47,12 @@ export const Leaderboard: React.FC = () => {
         </IonRow>
         <IonGrid className="leaderboard__table">
           <IonRow className="leaderboard__table__head">
-            <IonCol
-              className="leaderboard__table__head__user align-center"
-              size="6"
-            >
+            <IonCol className="leaderboard__table__head__user align-center">
               Benutzer
             </IonCol>
             <IonCol
               className="leaderboard__table__head__coins align-center"
               size="2"
-              offset="4"
             >
               Coins
             </IonCol>
