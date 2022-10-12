574c327097ccb3abee13bd39cfed4242707c18a8
Behemoth <simonostini@gmail.com>
2022-10-12 15:00:16 +0200

    removed guests from leaderboard

--- libs/server/controllers/leaderboard/src/lib/leaderboard.service.ts
+++ libs/server/controllers/leaderboard/src/lib/leaderboard.service.ts
@@ -1,6 +1,7 @@
 import { PrismaService } from '@bregenz-bewegt/server-prisma';
 import { Competitor } from '@bregenz-bewegt/shared/types';
 import { Injectable } from '@nestjs/common';
+import { Role } from '@prisma/client';
 
 @Injectable()
 export class LeaderboardService {
@@ -8,6 +9,7 @@ export class LeaderboardService {
 
   async getLeaderboard(): Promise<Competitor[]> {
     return this.prismaService.user.findMany({
+      where: { role: { not: Role.GUEST } },
       select: { username: true, coins: true },
       orderBy: { coins: 'desc' },
     });
