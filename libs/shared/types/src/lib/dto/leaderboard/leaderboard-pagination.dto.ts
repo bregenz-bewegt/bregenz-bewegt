e78975326dec7d74e6c9d20302918a0598c0eb2b
Behemoth <simonostini@gmail.com>
2022-10-13 12:28:43 +0200

    update

--- libs/shared/types/src/lib/dto/leaderboard/leaderboard-pagination.dto.ts
+++ libs/shared/types/src/lib/dto/leaderboard/leaderboard-pagination.dto.ts
@@ -1,13 +1,10 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsOptional } from 'class-validator';
 
 export class LeaderboardPaginationQueryDto {
-  @ApiProperty()
   @IsOptional()
   @IsNumber()
   skip?: number;
 
-  @ApiProperty()
   @IsOptional()
   @IsNumber()
   take?: number;
