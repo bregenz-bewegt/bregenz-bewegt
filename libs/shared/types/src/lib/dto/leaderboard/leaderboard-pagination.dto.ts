741e76cb929a160b65e1636254524685b4ff2c1f
Behemoth <simonostini@gmail.com>
2022-10-13 12:01:56 +0200

    fix

--- libs/shared/types/src/lib/dto/leaderboard/leaderboard-pagination.dto.ts
+++ libs/shared/types/src/lib/dto/leaderboard/leaderboard-pagination.dto.ts
@@ -1,13 +1,10 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, Min } from 'class-validator';
 
 export class LeaderboardPaginationDto {
-  @ApiProperty()
   @IsNumber()
   @Min(0)
   skip: number;
 
-  @ApiProperty()
   @IsNumber()
   @Min(1)
   take: number;
