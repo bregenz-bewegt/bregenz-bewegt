5c645758bba3b5403615cda065e9e57c2abdaa6a
Behemoth <simonostini@gmail.com>
2022-10-15 14:40:08 +0200

    test

--- libs/client/ui/pages/src/lib/leaderboard/leaderboard.tsx
+++ libs/client/ui/pages/src/lib/leaderboard/leaderboard.tsx
@@ -125,7 +125,7 @@ export const Leaderboard: React.FC<LeaderboardProps> = inject(
           {leaderboard.some(
             (competitor) => competitor.username === userStore?.user?.username
           ) && (
-            <IonRow className={``}>
+            <IonRow className={`self__not-in-top`}>
               <IonCol size="2" className={`align-center`}>
                 <div className={`rank-medal`}>X</div>
               </IonCol>
