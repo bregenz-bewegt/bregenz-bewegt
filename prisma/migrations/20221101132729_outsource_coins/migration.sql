fc91760dec9a2c855104d23bd8fa415ba19cf7bf
Behemoth <simonostini@gmail.com>
2022-11-01 14:28:09 +0100

    update

--- prisma/migrations/20221101132729_outsource_coins/migration.sql
+++ prisma/migrations/20221101132729_outsource_coins/migration.sql
@@ -1,11 +1,17 @@
 /*
   Warnings:
 
+  - The primary key for the `activity` table will be changed. If it partially fails, the table could be left without primary key constraint.
   - You are about to drop the column `points` on the `exercise` table. All the data in the column will be lost.
   - You are about to drop the column `coins` on the `user` table. All the data in the column will be lost.
   - Added the required column `coins` to the `Exercise` table without a default value. This is not possible if the table is not empty.
 
 */
+-- AlterTable
+ALTER TABLE `activity` DROP PRIMARY KEY,
+    MODIFY `id` VARCHAR(191) NOT NULL,
+    ADD PRIMARY KEY (`id`);
+
 -- AlterTable
 ALTER TABLE `exercise` DROP COLUMN `points`,
     ADD COLUMN `coins` INTEGER NOT NULL;
