60c26c9d005e20fd07b495503d8a22fb08591fe8
Behemoth <simonostini@gmail.com>
2022-11-01 15:23:15 +0100

    update

--- libs/shared/types/src/lib/dto/activity/end-activity.dto.ts
+++ libs/shared/types/src/lib/dto/activity/end-activity.dto.ts
@@ -1,7 +1,8 @@
 import { Activity } from '@prisma/client';
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class EndActivityDto {
   @IsString()
+  @IsNotEmpty()
   activityId: Activity['id'];
 }
