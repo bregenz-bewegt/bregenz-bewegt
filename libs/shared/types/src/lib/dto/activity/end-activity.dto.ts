a77f0153865b15d6e73976c8ed9bb596fd9ebf47
Behemoth <simonostini@gmail.com>
2022-11-01 14:40:06 +0100

    update

--- libs/shared/types/src/lib/dto/activity/end-activity.dto.ts
+++ libs/shared/types/src/lib/dto/activity/end-activity.dto.ts
@@ -1,12 +1,7 @@
-import { Exercise, Park } from '@prisma/client';
-import { IsNumber, IsPositive } from 'class-validator';
+import { Activity } from '@prisma/client';
+import { IsString } from 'class-validator';
 
 export class EndActivityDto {
-  @IsNumber()
-  @IsPositive()
-  exerciseId: Exercise['id'];
-
-  @IsNumber()
-  @IsPositive()
-  parkId: Park['id'];
+  @IsString()
+  activityId: Activity['id'];
 }
