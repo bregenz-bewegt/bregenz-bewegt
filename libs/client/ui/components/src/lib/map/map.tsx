8dc25af667dc006583069160ec3ba6c3226933a8
Timon Stadelmann <stadelmann.timon@gmail.com>
2022-11-05 23:32:56 +0100

    first map integration - with google maps

--- libs/client/ui/components/src/lib/map/map.tsx
+++ libs/client/ui/components/src/lib/map/map.tsx
@@ -1,6 +1,7 @@
 import { Park } from '@bregenz-bewegt/client/types';
 import { useEffect, useState } from 'react';
 import './map.scss';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
 export interface MapProps {
   parks: Park[];
@@ -12,16 +13,23 @@ export const Map: React.FC<MapProps> = ({ parks }: MapProps) => {
     setParkPins(parks);
   }, [parks]);
   return (
-    <div>
+    <div className="map">
       <p>In Entwicklung - Maps</p>
-      {parkPins &&
-        parkPins[1].id !== 0 &&
-        JSON.stringify(
-          parkPins.map((p) => [
-            p.coordinates && p.coordinates.latitude,
-            p.coordinates && p.coordinates.longitude,
-          ])
-        )}
+      <MapContainer
+        center={[47.491571, 9.723747]}
+        zoom={15}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
+          attribution='&copy; <a href="https://www.google.com/intl/de_at/help/terms_maps/">Google Maps</a> contributors'
+          maxZoom={20}
+          subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
+        />
+        <Marker position={[47.491571, 9.723747]}>
+          <Popup>Test - HAK Bregenz</Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 };
