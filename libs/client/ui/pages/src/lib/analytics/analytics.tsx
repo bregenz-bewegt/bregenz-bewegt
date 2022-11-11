51a4912c06cf4d499521f83109465faf5b67b428
Timon Stadelmann <stadelmann.timon@gmail.com>
2022-11-11 07:33:29 +0100

    started

--- libs/client/ui/pages/src/lib/analytics/analytics.tsx
+++ libs/client/ui/pages/src/lib/analytics/analytics.tsx
@@ -1,12 +1,19 @@
 import { Header } from '@bregenz-bewegt/client-ui-components';
-import { IonContent, IonPage } from '@ionic/react';
+import { IonContent, IonPage, IonText } from '@ionic/react';
 import './analytics.scss';
 
 export const Analytics: React.FC = () => {
   return (
-    <IonPage>
+    <IonPage className="analytics">
       <Header />
-      <IonContent className="analytics" fullscreen></IonContent>
+      <IonContent className="analytics__content" fullscreen>
+        <IonText className="analytics__content__title">
+          <h2>Statistik</h2>
+        </IonText>
+        <IonText>
+          <h2>Aktivitäts Verlauf</h2>
+        </IonText>
+      </IonContent>
     </IonPage>
   );
 };
