ee778fab1ac1dd7ed7042dbe63e3cf8b1457cb50
Behemoth <simonostini@gmail.com>
2022-11-11 15:59:18 +0100

    fix lint

--- libs/shared/types/src/lib/dto/auth/register.dto.ts
+++ libs/shared/types/src/lib/dto/auth/register.dto.ts
@@ -1,11 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsEmail,
-  IsNotEmpty,
-  IsOptional,
-  IsString,
-  Length,
-} from 'class-validator';
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class RegisterDto {
   @ApiProperty({ required: false })
