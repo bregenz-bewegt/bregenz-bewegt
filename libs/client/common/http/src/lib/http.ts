fc1973efd150b5ec62ec782fafa72ac5774a2b03
Behemoth <simonostini@gmail.com>
2022-11-14 17:39:32 +0100

    update

--- libs/client/common/http/src/lib/http.ts
+++ libs/client/common/http/src/lib/http.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
-import {
-  createResponseInterceptor,
-  createRequestInterceptor,
-} from './interceptors';
+import createAuthRefreshInterceptor from 'axios-auth-refresh';
+import { userStore } from '@bregenz-bewegt/client/common/stores';
+import { createRequestInterceptor } from './interceptors';
 
 const http = axios.create({
   baseURL: `${process.env['NX_API_BASE_URL']}/api`,
@@ -10,6 +9,34 @@ const http = axios.create({
 });
 
 createRequestInterceptor(http);
-createResponseInterceptor(http);
+createAuthRefreshInterceptor(
+  http,
+  async (error) => {
+    const { refresh_token } = await userStore.getTokens();
+
+    return http
+      .post(
+        '/auth/refresh',
+        {},
+        {
+          headers: {
+            authorization: `Bearer ${refresh_token}`,
+          },
+        }
+      )
+      .then(async (response) => {
+        await userStore.setTokens(response.data);
+        error.response.config.headers['authorization'] =
+          'Bearer ' + response.data.access_token;
+        return Promise.resolve();
+      })
+      .catch(async (error) => {
+        await userStore.removeTokens();
+        return Promise.reject(error);
+      });
+  },
+  { statusCodes: [401] }
+);
+// createResponseInterceptor(http);
 
 export { http };
--- libs/server/controllers/auth/src/lib/auth.service.ts
+++ libs/server/controllers/auth/src/lib/auth.service.ts
@@ -223,7 +223,7 @@ export class AuthService {
 
     const [access_token, refresh_token] = await Promise.all([
       this.jwtService.signAsync(jwtPayload, {
-        expiresIn: '15m',
+        expiresIn: '3s',
         secret: this.configService.get('NX_JWT_ACCESS_TOKEN_SECRET'),
       }),
       this.jwtService.signAsync(jwtPayload, {
