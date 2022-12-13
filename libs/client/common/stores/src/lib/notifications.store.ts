06f3377a35f420d0977959db75135deabf8718d3
Behemoth <simonostini@gmail.com>
2022-12-13 17:05:29 +0100

    rm log

--- libs/client/common/stores/src/lib/notifications.store.ts
+++ libs/client/common/stores/src/lib/notifications.store.ts
@@ -37,7 +37,6 @@ export class NotificationsStore implements Store {
 
   @action async fetchNotifications(): Promise<Notification[]> {
     const { data }: { data: Notification[] } = await http.get('notifications');
-    console.log(data);
 
     this.setNotifications(data);
     return data;
