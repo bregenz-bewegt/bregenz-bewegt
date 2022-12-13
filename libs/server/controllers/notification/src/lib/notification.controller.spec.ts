ee3b6db134444c76ada04cc03ffbf57760380898
Behemoth <simonostini@gmail.com>
2022-12-13 19:06:04 +0100

    fix test

--- libs/server/controllers/notification/src/lib/notification.controller.spec.ts
+++ libs/server/controllers/notification/src/lib/notification.controller.spec.ts
@@ -1,3 +1,5 @@
+import { PrismaService } from '@bregenz-bewegt/server-prisma';
+import { ConfigService } from '@nestjs/config';
 import { Test } from '@nestjs/testing';
 import { NotificationController } from './notification.controller';
 import { NotificationService } from './notification.service';
@@ -7,7 +9,7 @@ describe('NotificationController', () => {
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
-      providers: [NotificationService],
+      providers: [NotificationService, PrismaService, ConfigService],
       controllers: [NotificationController],
     }).compile();
 
--- libs/server/controllers/notification/src/lib/notification.service.spec.ts
+++ libs/server/controllers/notification/src/lib/notification.service.spec.ts
@@ -1,3 +1,5 @@
+import { PrismaService } from '@bregenz-bewegt/server-prisma';
+import { ConfigService } from '@nestjs/config';
 import { Test } from '@nestjs/testing';
 import { NotificationService } from './notification.service';
 
@@ -6,7 +8,7 @@ describe('NotificationService', () => {
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
-      providers: [NotificationService],
+      providers: [NotificationService, PrismaService, ConfigService],
     }).compile();
 
     service = module.get(NotificationService);
