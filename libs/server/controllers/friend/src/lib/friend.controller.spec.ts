1ce09352e68e3dd60b73134523e0a4686cf71d2d
Behemoth <simonostini@gmail.com>
2022-12-16 09:38:12 +0100

    fix test

--- libs/server/controllers/friend/src/lib/friend.controller.spec.ts
+++ libs/server/controllers/friend/src/lib/friend.controller.spec.ts
@@ -1,6 +1,7 @@
 import { PrismaService } from '@bregenz-bewegt/server-prisma';
 import { ConfigService } from '@nestjs/config';
 import { Test } from '@nestjs/testing';
+import { NotificationGateway } from 'libs/server/gateway/src/lib/gateways';
 import { FriendController } from './friend.controller';
 import { FriendService } from './friend.service';
 
@@ -9,7 +10,12 @@ describe('FriendController', () => {
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
-      providers: [FriendService, PrismaService, ConfigService],
+      providers: [
+        FriendService,
+        PrismaService,
+        ConfigService,
+        NotificationGateway,
+      ],
       controllers: [FriendController],
     }).compile();
 
