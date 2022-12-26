317d459efc47ff05647b75549232e41394546a5f
Behemoth <simonostini@gmail.com>
2022-12-26 18:22:00 +0100

    update

--- libs/client/ui/components/src/lib/input/input.tsx
+++ libs/client/ui/components/src/lib/input/input.tsx
@@ -26,62 +26,6 @@ export interface InputProps {
   error?: string;
   expand?: boolean;
   clearOnEdit?: boolean;
-  autocomplete?:
-    | 'on'
-    | 'off'
-    | 'name'
-    | 'honorific-prefix'
-    | 'given-name'
-    | 'additional-name'
-    | 'family-name'
-    | 'honorific-suffix'
-    | 'nickname'
-    | 'email'
-    | 'username'
-    | 'new-password'
-    | 'current-password'
-    | 'one-time-code'
-    | 'organization-title'
-    | 'organization'
-    | 'street-address'
-    | 'address-line1'
-    | 'address-line2'
-    | 'address-line3'
-    | 'address-level4'
-    | 'address-level3'
-    | 'address-level2'
-    | 'address-level1'
-    | 'country'
-    | 'country-name'
-    | 'postal-code'
-    | 'cc-name'
-    | 'cc-given-name'
-    | 'cc-additional-name'
-    | 'cc-family-name'
-    | 'cc-family-name'
-    | 'cc-number'
-    | 'cc-exp'
-    | 'cc-exp-month'
-    | 'cc-exp-year'
-    | 'cc-csc'
-    | 'cc-type'
-    | 'transaction-currency'
-    | 'transaction-amount'
-    | 'language'
-    | 'bday'
-    | 'bday-day'
-    | 'bday-month'
-    | 'bday-year'
-    | 'sex'
-    | 'tel'
-    | 'tel-country-code'
-    | 'tel-national'
-    | 'tel-area-code'
-    | 'tel-local'
-    | 'tel-extension'
-    | 'impp'
-    | 'url'
-    | 'photo';
   className?: string;
   ref?: React.Ref<HTMLIonInputElement>;
   onChange?: (event: IonInputCustomEvent<InputChangeEventDetail>) => void;
@@ -97,7 +41,6 @@ export const Input: React.FC<InputProps> = ({
   required,
   disabled,
   label,
-  autocomplete = 'email',
   error,
   expand = true,
   className,
@@ -115,7 +58,6 @@ export const Input: React.FC<InputProps> = ({
     required,
     disabled,
     clearOnEdit,
-    autocomplete,
     ref,
     onIonChange: onChange,
     onIonBlur: onBlur,
