17a90b3a94552de5f2f556f4ab3735ea3f9c88e0
Behemoth <simonostini@gmail.com>
2023-01-04 15:18:06 +0100

    rm logs fix lint

--- libs/client/ui/pages/src/lib/exercise-detail/exercise-detail.tsx
+++ libs/client/ui/pages/src/lib/exercise-detail/exercise-detail.tsx
@@ -62,8 +62,6 @@ export const ExerciseDetail: React.FC<ExerciseDetailProps> = inject(
       const location = await locationStore?.getLocation();
       if (!location?.coords || !coordinates) return setIsLocationValid(false);
 
-      console.log(coordinates, location.coords);
-
       const isValid = locationStore?.isLocationWithinRadius(
         location.coords,
         {
@@ -72,8 +70,6 @@ export const ExerciseDetail: React.FC<ExerciseDetailProps> = inject(
         },
         coordinates?.toleranceRadius
       );
-
-      console.log(isValid);
 
       setIsLocationValid(isValid || false);
     };
