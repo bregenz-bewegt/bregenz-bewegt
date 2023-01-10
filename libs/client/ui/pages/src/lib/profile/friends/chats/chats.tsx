78ae60b4a3867740b8c2a5ce46271e51a0f8c024
Behemoth <simonostini@gmail.com>
2023-01-10 08:29:31 +0100

    update

--- libs/client/ui/pages/src/lib/profile/friends/chats/chats.tsx
+++ libs/client/ui/pages/src/lib/profile/friends/chats/chats.tsx
@@ -1,8 +1,20 @@
+import { IonGrid, IonRow, IonText, IonList } from '@ionic/react';
 import React from 'react';
 import './chats.scss';
 
 export interface ChatProps {}
 
 export const Chats: React.FC<ChatProps> = ({}) => {
-  return <div className="chats">chats</div>;
+  return (
+    <div className="chats">
+      <IonGrid>
+        <IonRow>
+          <IonText>
+            <h2>Chats</h2>
+          </IonText>
+        </IonRow>
+        <IonList className=""></IonList>
+      </IonGrid>
+    </div>
+  );
 };
