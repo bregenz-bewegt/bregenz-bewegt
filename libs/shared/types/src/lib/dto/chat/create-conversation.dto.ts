cf5eba8cf19d1776655e2e2d109f08f79f813792
Behemoth <simonostini@gmail.com>
2023-01-10 16:36:18 +0100

    update

--- libs/shared/types/src/lib/dto/chat/create-conversation.dto.ts
+++ libs/shared/types/src/lib/dto/chat/create-conversation.dto.ts
@@ -1,9 +1,8 @@
-import { User } from '@bregenz-bewegt/client/types';
 import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreateConversationDto {
   @ApiProperty()
   @IsString({ each: true })
-  participants: User['username'][];
+  participants: string[];
 }
