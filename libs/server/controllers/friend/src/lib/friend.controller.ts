60ea8898880a010ae497f0d47996e65a4f627aef
Behemoth <simonostini@gmail.com>
2023-01-10 12:39:41 +0100

    fix

--- libs/server/controllers/friend/src/lib/friend.controller.ts
+++ libs/server/controllers/friend/src/lib/friend.controller.ts
@@ -86,7 +86,7 @@ export class FriendController {
     )
     dto: SearchUserQueryDto
   ): Promise<FriendSearchResult[]> {
-    return this.friendService.searchUserByUsername(dto.username, userId);
+    return this.friendService.searchUserByUsername(dto.username, userId, true);
   }
 
   @HasRole(Role.USER)
