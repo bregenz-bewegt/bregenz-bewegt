dd7295627e96f69435c305615ec56824af000f1b
Behemoth <simonostini@gmail.com>
2023-01-10 16:24:21 +0100

    fix

--- libs/shared/types/src/lib/dto/chat/create-conversation.dto.ts
+++ libs/shared/types/src/lib/dto/chat/create-conversation.dto.ts
@@ -1,10 +1,9 @@
-import { FriendRequest } from '@bregenz-bewegt/client/types';
+import { Conversation } from '@bregenz-bewegt/client/types';
 import { ApiProperty } from '@nestjs/swagger';
-import { User } from '@prisma/client';
 import { IsString } from 'class-validator';
 
 export class CreateConversationDto {
   @ApiProperty()
   @IsString({ each: true })
-  participants:
+  participants: Conversation['participants'];
 }
