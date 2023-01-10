93ac620d10176f095a5db0a407057f76e3d0cac2
Behemoth <simonostini@gmail.com>
2023-01-10 09:43:32 +0100

    update

--- libs/server/controllers/chat/src/lib/chat.controller.ts
+++ libs/server/controllers/chat/src/lib/chat.controller.ts
@@ -5,7 +5,7 @@ import { ChatService } from './chat.service';
 export class ChatController {
   constructor(private chatService: ChatService) {}
 
-  @Get()
+  @Get(':username')
   async getConversation(): Promise<void> {
     return void 0;
   }
