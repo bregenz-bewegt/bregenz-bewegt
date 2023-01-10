408b93b5b91f91232d02aef482c4b0c44edf563b
Behemoth <simonostini@gmail.com>
2023-01-10 08:28:05 +0100

    fix

--- libs/client/ui/pages/src/lib/profile/friends/chats/chats.tsx
+++ libs/client/ui/pages/src/lib/profile/friends/chats/chats.tsx
@@ -1,30 +1,8 @@
-import { BackButton } from '@bregenz-bewegt/client-ui-components';
-import { tabRoutes } from '@bregenz-bewegt/client-ui-router';
-import {
-  IonPage,
-  IonHeader,
-  IonToolbar,
-  IonButtons,
-  IonTitle,
-  IonContent,
-} from '@ionic/react';
 import React from 'react';
 import './chats.scss';
 
 export interface ChatProps {}
 
 export const Chats: React.FC<ChatProps> = ({}) => {
-  return (
-    <IonPage className="chats">
-      <IonHeader mode="ios" collapse="condense" className="ion-no-border">
-        <IonToolbar>
-          <IonButtons>
-            <BackButton defaultRouterLink={tabRoutes.profile.route} />
-          </IonButtons>
-          <IonTitle>Freunde</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="chats__content" fullscreen></IonContent>
-    </IonPage>
-  );
+  return <div className="chats">chats</div>;
 };
