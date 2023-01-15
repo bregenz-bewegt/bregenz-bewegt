d23401001ffe5e0a8d57d822c74586019dd84e45
Behemoth <simonostini@gmail.com>
2023-01-15 19:46:55 +0100

    update

--- libs/client/ui/pages/src/lib/profile/terms-of-service/terms-of-service.tsx
+++ libs/client/ui/pages/src/lib/profile/terms-of-service/terms-of-service.tsx
@@ -11,6 +11,7 @@ import {
 } from '@ionic/react';
 import { tabRoutes } from '@bregenz-bewegt/client-ui-router';
 import { UserStore } from '@bregenz-bewegt/client/common/stores';
+import { BackButton } from '@bregenz-bewegt/client-ui-components';
 
 export interface TermsOfServiceProps {
   userStore?: UserStore;
@@ -19,16 +20,12 @@ export interface TermsOfServiceProps {
 export const TermsOfService: React.FC<TermsOfServiceProps> = () => {
   return (
     <IonPage className="terms-of-service">
-      <IonHeader mode="ios">
+      <IonHeader mode="ios" collapse="condense" className="ion-no-border">
         <IonToolbar>
           <IonButtons>
-            <IonBackButton
-              color="primary"
-              defaultHref={tabRoutes.profile.route}
-              text="Zurück"
-            />
+            <BackButton defaultRouterLink={tabRoutes.profile.route} />
           </IonButtons>
-          <IonTitle>Nutzungsbedingungen</IonTitle>
+          <IonTitle>Darstellung</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent className="terms-of-service__content"></IonContent>
