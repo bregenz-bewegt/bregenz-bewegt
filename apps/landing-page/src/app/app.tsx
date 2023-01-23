e3b399235ea2e9c1928eb5979aba025eefa89cb0
Behemoth <simonostini@gmail.com>
2023-01-23 12:56:51 +0100

    add landing page app

---
+++ apps/landing-page/src/app/app.tsx
@@ -0,0 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from 'react';
+import './app.module.scss';
+
+export const App: React.FC = () => {
+  return <div className="app">app</div>;
+};
