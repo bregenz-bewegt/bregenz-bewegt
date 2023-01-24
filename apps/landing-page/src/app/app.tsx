319c68d5aaeca09578dcee1aeffcccd5e367a27c
Behemoth <simonostini@gmail.com>
2023-01-24 17:39:59 +0100

    update

--- apps/landing-page/src/app/app.tsx
+++ apps/landing-page/src/app/app.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Hero } from '@bregenz-bewegt/landing-page/components';
+import { Footer, Hero } from '@bregenz-bewegt/landing-page/components';
 import './app.module.scss';
 
 export const App: React.FC = () => {
   return (
     <div className="app">
       <Hero />
+      <Footer />
     </div>
   );
 };
---
+++ libs/landing-page/components/src/lib/footer/footer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './footer.scss';
+
+export interface FooterProps {}
+
+export const Footer: React.FC<FooterProps> = (props: FooterProps) => {
+  return <footer className="footer">test</footer>;
+};
