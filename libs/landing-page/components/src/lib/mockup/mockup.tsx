4aa5139d67aa44382eef71cff4b3812fc5d9d23c
Behemoth <simonostini@gmail.com>
2023-01-24 16:45:31 +0100

    update

--- libs/landing-page/components/src/lib/mockup/mockup.tsx
+++ libs/landing-page/components/src/lib/mockup/mockup.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './mockup.scss';
-import phone from './assets/phone.png';
 
 export interface MockupProps {
   src: string;
@@ -9,7 +8,17 @@ export interface MockupProps {
 export const Mockup: React.FC<MockupProps> = ({ src }) => {
   return (
     <div className="mockup">
-      <iframe src={src} title="web app mockup"></iframe>;
+      <div className="mockup__notch">
+        <span className="mockup__notch__speaker"></span>
+      </div>
+      <div className="mockup__screen">
+        <iframe
+          className="mockup__screen__iframe"
+          src={src}
+          title="web app mockup"
+        ></iframe>
+      </div>
+      <div className="mockup__home-indicator"></div>
     </div>
   );
 };
