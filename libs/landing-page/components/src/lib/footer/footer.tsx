55fed878bfa711109a924242b01ff1a6224a3b1a
Behemoth <simonostini@gmail.com>
2023-01-24 18:25:41 +0100

    fix lint

--- libs/landing-page/components/src/lib/footer/footer.tsx
+++ libs/landing-page/components/src/lib/footer/footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './footer.scss';
 
+/* eslint-disable-next-line */
 export interface FooterProps {}
 
 export const Footer: React.FC<FooterProps> = (props: FooterProps) => {
