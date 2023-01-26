f25b344c1e95092f3495b358c0ff4960e03ce7b9
Behemoth <simonostini@gmail.com>
2023-01-26 18:46:22 +0100

    udpate

--- libs/landing-page/components/src/lib/hero/hero.tsx
+++ libs/landing-page/components/src/lib/hero/hero.tsx
@@ -13,7 +13,21 @@ export const Hero: React.FC<HeroProps> = (props: HeroProps) => {
     gsap.fromTo(
       '.hero__info__text-part__hidden',
       { y: '100%' },
-      { y: 0, duration: 1, stagger: 0.2, ease: 'power4.easeOut' }
+      { y: 0, duration: 1, stagger: 0.15, ease: 'power4.easeOut' }
+    );
+
+    gsap.fromTo(
+      '.hero__bow',
+      {
+        opacity: 0,
+        height: '0%',
+      },
+      {
+        opacity: 1,
+        height: '30%',
+        delay: 1,
+        duration: 1,
+      }
     );
   }, []);
 
@@ -23,9 +37,15 @@ export const Hero: React.FC<HeroProps> = (props: HeroProps) => {
         <Mockup src={`${process.env['NX_CLIENT_BASE_URL']}`} />
       </div>
       <div className="hero__info">
-        {titleSnippets.map((s) => (
+        {titleSnippets.map((snippet) => (
           <h1 className="hero__info__text-part">
-            <span className="hero__info__text-part__hidden">{s}</span>
+            <span className="hero__info__text-part__hidden">
+              {snippet.split('').map((letter) => (
+                <span className="hero__info__text-part__hidden__letter">
+                  {letter}
+                </span>
+              ))}
+            </span>
           </h1>
         ))}
       </div>
