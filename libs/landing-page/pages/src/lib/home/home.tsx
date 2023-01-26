2c3793bce9f83933fbe9514c0566cf946afbf349
Behemoth <simonostini@gmail.com>
2023-01-26 12:02:09 +0100

    update

--- libs/landing-page/pages/src/lib/home/home.tsx
+++ libs/landing-page/pages/src/lib/home/home.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './home.scss';
 import { Hero } from '@bregenz-bewegt/landing-page/components';
+import gsap from 'gsap';
 
 export const Home: React.FC = () => {
+  useEffect(() => {
+    gsap.fromTo('.home', {}, {});
+  }, []);
+
   return (
     <div className="home">
       <Hero />
