f2921abeacda958fc7d6ac87a6ff5d9d3705ed04
Behemoth <simonostini@gmail.com>
2023-01-26 12:03:22 +0100

    update

--- libs/landing-page/components/src/lib/hero/hero.tsx
+++ libs/landing-page/components/src/lib/hero/hero.tsx
@@ -1,17 +1,26 @@
+import React, { useEffect } from 'react';
 import { Mockup } from '../mockup/mockup';
 import './hero.scss';
+import gsap from 'gsap';
 
 /* eslint-disable-next-line */
 export interface HeroProps {}
 
 export const Hero: React.FC<HeroProps> = (props: HeroProps) => {
+  useEffect(() => {
+    gsap.fromTo(
+      '.hero__info__text',
+      { x: -100, opacity: 0 },
+      { x: 0, opacity: 1, duration: 2, ease: 'power4' }
+    );
+  }, []);
   return (
     <div className="hero">
       <div className="hero__mockup">
         <Mockup src={`${process.env['NX_CLIENT_BASE_URL']}`} />
       </div>
       <div className="hero__info">
-        <h1>
+        <h1 className="hero__info__text">
           Bregenz <br />
           Bewegt <br />
           App
