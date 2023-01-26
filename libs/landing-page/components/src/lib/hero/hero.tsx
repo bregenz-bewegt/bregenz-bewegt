16d2deef3f7192cb7ffe8d609f47c6340d3b3ef9
Behemoth <simonostini@gmail.com>
2023-01-26 12:13:56 +0100

    update

--- libs/landing-page/components/src/lib/hero/hero.tsx
+++ libs/landing-page/components/src/lib/hero/hero.tsx
@@ -9,9 +9,9 @@ export interface HeroProps {}
 export const Hero: React.FC<HeroProps> = (props: HeroProps) => {
   useEffect(() => {
     gsap.fromTo(
-      '.hero__info__text',
-      { x: -100, opacity: 0 },
-      { x: 0, opacity: 1, duration: 2, ease: 'power4' }
+      '.hero__info__text-part',
+      { x: 100, opacity: 0 },
+      { x: 0, opacity: 1, duration: 2, stagger: 0.1, ease: 'power4' }
     );
   }, []);
 
@@ -21,11 +21,9 @@ export const Hero: React.FC<HeroProps> = (props: HeroProps) => {
         <Mockup src={`${process.env['NX_CLIENT_BASE_URL']}`} />
       </div>
       <div className="hero__info">
-        <h1 className="hero__info__text">
-          Bregenz <br />
-          Bewegt <br />
-          App
-        </h1>
+        <h1 className="hero__info__text-part">Bregenz</h1>
+        <h1 className="hero__info__text-part">Bewegt</h1>
+        <h1 className="hero__info__text-part">App</h1>
       </div>
     </div>
   );
