d72178019277770847d7b1d8f7dc9876e40351f0
simonostini <simonostini@gmail.com>
2023-02-04 01:19:42 +0100

    Redirect guest forbidden routes

--- libs/client/ui/router/src/lib/private-tabs-outlet/private-tabs-outlet.tsx
+++ libs/client/ui/router/src/lib/private-tabs-outlet/private-tabs-outlet.tsx
@@ -24,10 +24,14 @@ import {
   IonFabButton,
 } from '@ionic/react';
 import { inject, observer } from 'mobx-react';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect, RouteComponentProps } from 'react-router-dom';
 import { tabRoutes } from '../tabs';
 import { ScanBarcode } from 'iconsax-react';
-import { NotificationListener } from '@bregenz-bewegt/client-ui-components';
+import {
+  NotificationListener,
+  RoleRouteGuard,
+} from '@bregenz-bewegt/client-ui-components';
+import { Role } from '@bregenz-bewegt/client/types';
 
 export interface PrivateTabsOutletProps {
   tabStore?: TabStore;
@@ -89,12 +93,20 @@ export const PrivateTabsOutlet: React.FC<PrivateTabsOutletProps> = inject(
             <Route
               exact
               path={`/user/:id`}
-              component={CompetitorProfile}
+              component={(props: RouteComponentProps<any>) => (
+                <RoleRouteGuard allowedRoles={[Role.USER]}>
+                  <CompetitorProfile {...props} />
+                </RoleRouteGuard>
+              )}
             ></Route>
             <Route
               exact
               path={`${tabRoutes.profile.route}/appearance`}
-              component={Appearance}
+              component={() => (
+                <RoleRouteGuard allowedRoles={[Role.USER]}>
+                  <Appearance />
+                </RoleRouteGuard>
+              )}
             ></Route>
             <Route
               exact
